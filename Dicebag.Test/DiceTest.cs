ce83a333642778dc24c19a58d13e2626e5fcbec7
istarnion <hallgeir@ablemagic.no>
2021-03-24 22:00:38 +0100

    Initial commit
    Setup project structure.
    Define outwards API (not stable ofc).
    Begin test suite.

---
+++ Dicebag.Test/DiceTest.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+
+namespace Dicebag.Test
+{
+    public class DiceTests
+    {
+        [Test]
+        public void SingleDigitNumber()
+        {
+            string expression = "1";
+            DiceRoller.Result result = DiceRoller.Roll(expression);
+            Assert.AreEqual(1, result.Total);
+            Assert.AreEqual(1, result.Modifier);
+            Assert.Equals(0, result.Rolls.Count);
+        }
+
+        [Test]
+        public void MultiDigitNumber()
+        {
+            string expression = "2895";
+            DiceRoller.Result result = DiceRoller.Roll(expression);
+            Assert.AreEqual(2895, result.Total);
+            Assert.AreEqual(2895, result.Modifier);
+            Assert.Equals(0, result.Rolls.Count);
+        }
+
+        [Test]
+        public void SimpleRoll()
+        {
+            string expression = "1d20";
+            DiceRoller.Result result = DiceRoller.Roll(expression);
+            Assert.Less(0, result.Total);
+            Assert.Greater(20, result.Total);
+            Assert.AreEqual(0, result.Modifier);
+            Assert.Equals(1, result.Rolls.Count);
+        }
+
+        [Test]
+        public void SimpleRollShorthand()
+        {
+            string expression = "d20";
+            DiceRoller.Result result = DiceRoller.Roll(expression);
+            Assert.Less(0, result.Total);
+            Assert.Greater(20, result.Total);
+            Assert.AreEqual(0, result.Modifier);
+            Assert.Equals(1, result.Rolls.Count);
+        }
+
+        [Test]
+        public void RollWithModifier()
+        {
+            string expression = "1d20+5";
+            DiceRoller.Result result = DiceRoller.Roll(expression);
+            Assert.Less(5, result.Total);
+            Assert.Greater(25, result.Total);
+            Assert.AreEqual(5, result.Modifier);
+            Assert.Equals(1, result.Rolls.Count);
+        }
+
+        [Test]
+        public void CompondRoll()
+        {
+            string expression = "1d20+2d6";
+            DiceRoller.Result result = DiceRoller.Roll(expression);
+            Assert.Less(1, result.Total);
+            Assert.Greater(27, result.Total);
+            Assert.AreEqual(0, result.Modifier);
+            Assert.Equals(2, result.Rolls.Count);
+        }
+
+        [Test]
+        public void CompondRollWithShorthands()
+        {
+            string expression = "d12+d8";
+            DiceRoller.Result result = DiceRoller.Roll(expression);
+            Assert.Less(1, result.Total);
+            Assert.Greater(21, result.Total);
+            Assert.AreEqual(0, result.Modifier);
+            Assert.Equals(2, result.Rolls.Count);
+        }
+    }
+}
+
---
+++ Dicebag/DiceRoller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dicebag
+{
+    /// <summary>
+    /// Simple too for rolling dice expressions such as 1d20, 4d6+2 etc
+    /// </summary>
+    /// <remarks>
+    /// Dice expression BNF:
+    ///            [roll] ::= [expression]((+|-)[expression])*
+    ///      [expression] ::= [number]|[dice-expression]
+    /// [dice-expression] ::= [number]?d[number]
+    ///
+    /// Any whitespace is ignored
+    /// </remarks>
+    public static class DiceRoller
+    {
+        /// <summary>
+        /// Holds the result of a roll.
+        /// </summary>
+        public class Result
+        {
+            /// <summary>
+            /// The total sum of the roll
+            /// </summary>
+            public int Total { get; private set; }
+
+            /// <summary>
+            /// The total modifier that was applied to the roll(s)
+            /// </summary>
+            public int Modifier { get; private set; }
+
+            /// <summary>
+            /// The results of the individual rolls.
+            /// The key is the die type, "d20", "d6", etc
+            /// The value is a list of the results of every roll with that die
+            /// </summary>
+            public Dictionary<string, List<int>> Rolls { get; private set; }
+        }
+
+        public static Result Roll(string diceExpression)
+        {
+            if(string.IsNullOrWhiteSpace(diceExpression))
+            {
+                throw new Exception("[ERROR]: The roll expression is empty.");
+            }
+
+            diceExpression = diceExpression.Trim();
+            int offset = 0;
+
+            void Panic(string errorMessage)
+            {
+                string errorPosition = new string(' ', offset) + "^";
+                throw new Exception(string.Format("[ERROR]: ({0}) {1}\n\t{2}\n\t{3}", offset, errorMessage, diceExpression, errorPosition));
+            }
+
+
+
+            return null;
+        }
+    }
+}
+
