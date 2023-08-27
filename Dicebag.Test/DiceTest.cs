2ebf440d066ce4d59ac115bba4801405103d94e6
Ole Jørgen Skogstad <skogstad@softis.net>
2023-08-27 21:17:57 +0200

    Upgrade to .NET7
    Update NuGets
    Use file scoped namespace
    Use init
    Make classes sealed

--- Dicebag.Test/DiceTest.cs
+++ Dicebag.Test/DiceTest.cs
@@ -1,206 +1,199 @@
 using NUnit.Framework;
 
-namespace Dicebag.Test
+namespace Dicebag.Test;
+
+public class DiceTests
 {
-    public class DiceTests
+    private static void AssertRange(int total, int low, int high)
     {
-        private static void AssertRange(int total, int low, int high)
-        {
-           Assert.LessOrEqual(low, total);
-           Assert.GreaterOrEqual(high, total);
-        }
+        Assert.LessOrEqual(low, total);
+        Assert.GreaterOrEqual(high, total);
+    }
         
-        [Test]
-        public void SingleDigitNumber()
+    [Test]
+    public void SingleDigitNumber()
+    {
+        const string expression = "1";
+        var result = DiceRoller.Roll(expression);
+        Assert.AreEqual(1, result.Total);
+        Assert.AreEqual(1, result.Modifier);
+        Assert.AreEqual(0, result.Rolls.Count);
+    }
+
+    [Test]
+    public void MultiDigitNumber()
+    {
+        const string expression = "2895";
+        var result = DiceRoller.Roll(expression);
+        Assert.AreEqual(2895, result.Total);
+        Assert.AreEqual(2895, result.Modifier);
+        Assert.AreEqual(0, result.Rolls.Count);
+    }
+
+    [Test]
+    public void SimpleRoll()
+    {
+        const string expression = "1d20";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 1, 20);
+        Assert.AreEqual(0, result.Modifier);
+        Assert.AreEqual(1, result.Rolls.Count);
+    }
+
+    [Test]
+    public void SimpleRollShorthand()
+    {
+        const string expression = "d20";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 1, 20);
+        Assert.AreEqual(0, result.Modifier);
+        Assert.AreEqual(1, result.Rolls.Count);
+    }
+
+    [Test]
+    public void SimpleRollUppercase()
+    {
+        const string expression = "1D20";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 1, 20);
+        Assert.AreEqual(0, result.Modifier);
+        Assert.AreEqual(1, result.Rolls.Count);
+    }
+
+    [Test]
+    public void SimpleRollShorthandUppercase()
+    {
+        const string expression = "D20";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 1, 20);
+        Assert.AreEqual(0, result.Modifier);
+        Assert.AreEqual(1, result.Rolls.Count);
+    }
+
+
+    [Test]
+    public void RollWithModifier()
+    {
+        const string expression = "1d20+5";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 6, 25);
+        Assert.AreEqual(5, result.Modifier);
+        Assert.AreEqual(1, result.Rolls.Count);
+    }
+
+    [Test]
+    public void WhitespaceStress()
+    {
+        const string expression = "   1     d      20 +    5      ";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 6, 25);
+        Assert.AreEqual(5, result.Modifier);
+        Assert.AreEqual(1, result.Rolls.Count);
+    }
+
+    [Test]
+    public void CompoundRoll()
+    {
+        const string expression = "1d20+2d6";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 3, 32);
+        Assert.AreEqual(0, result.Modifier);
+        Assert.AreEqual(2, result.Rolls.Count);
+    }
+
+    [Test]
+    public void CompoundRollWithShorthands()
+    {
+        const string expression = "d12+d8";
+        var result = DiceRoller.Roll(expression);
+        AssertRange(result.Total, 2, 20);
+        Assert.AreEqual(0, result.Modifier);
+        Assert.AreEqual(2, result.Rolls.Count);
+    }
+
+    [Test]
+    public void EmptyInput()
+    {
+        const string expression = "";
+        try
         {
-            const string expression = "1";
             var result = DiceRoller.Roll(expression);
-            Assert.AreEqual(1, result.Total);
-            Assert.AreEqual(1, result.Modifier);
-            Assert.AreEqual(0, result.Rolls.Count);
-        }
-
-        [Test]
-        public void MultiDigitNumber()
-        {
-            const string expression = "2895";
-            var result = DiceRoller.Roll(expression);
-            Assert.AreEqual(2895, result.Total);
-            Assert.AreEqual(2895, result.Modifier);
-            Assert.AreEqual(0, result.Rolls.Count);
-        }
-
-        [Test]
-        public void SimpleRoll()
-        {
-            const string expression = "1d20";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 1, 20);
-            Assert.AreEqual(0, result.Modifier);
-            Assert.AreEqual(1, result.Rolls.Count);
-        }
-
-        [Test]
-        public void SimpleRollShorthand()
-        {
-            const string expression = "d20";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 1, 20);
-            Assert.AreEqual(0, result.Modifier);
-            Assert.AreEqual(1, result.Rolls.Count);
-        }
-
-        [Test]
-        public void SimpleRollUppercase()
-        {
-            const string expression = "1D20";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 1, 20);
-            Assert.AreEqual(0, result.Modifier);
-            Assert.AreEqual(1, result.Rolls.Count);
-        }
-
-        [Test]
-        public void SimpleRollShorthandUppercase()
-        {
-            const string expression = "D20";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 1, 20);
-            Assert.AreEqual(0, result.Modifier);
-            Assert.AreEqual(1, result.Rolls.Count);
-        }
-
-
-        [Test]
-        public void RollWithModifier()
-        {
-            const string expression = "1d20+5";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 6, 25);
-            Assert.AreEqual(5, result.Modifier);
-            Assert.AreEqual(1, result.Rolls.Count);
-        }
-
-        [Test]
-        public void WhitespaceStress()
-        {
-            const string expression = "   1     d      20 +    5      ";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 6, 25);
-            Assert.AreEqual(5, result.Modifier);
-            Assert.AreEqual(1, result.Rolls.Count);
-        }
-
-        [Test]
-        public void CompoundRoll()
-        {
-            const string expression = "1d20+2d6";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 3, 32);
-            Assert.AreEqual(0, result.Modifier);
-            Assert.AreEqual(2, result.Rolls.Count);
-        }
-
-        [Test]
-        public void CompoundRollWithShorthands()
-        {
-            const string expression = "d12+d8";
-            var result = DiceRoller.Roll(expression);
-            AssertRange(result.Total, 2, 20);
-            Assert.AreEqual(0, result.Modifier);
-            Assert.AreEqual(2, result.Rolls.Count);
-        }
-
-        [Test]
-        public void EmptyInput()
-        {
-            const string expression = "";
-            try
-            {
-                var result = DiceRoller.Roll(expression);
-                Assert.Fail();
-            }
-            catch (DiceRollException)
-            {
-                Assert.Pass();
-            }
-
             Assert.Fail();
         }
-
-        [Test]
-        public void NullInput()
+        catch (DiceRollException)
         {
-            const string expression = null;
-            try
-            {
-                var result = DiceRoller.Roll(expression);
-                Assert.Fail();
-            }
-            catch (DiceRollException)
-            {
-                Assert.Pass();
-            }
+            Assert.Pass();
+        }
+    }
 
+    [Test]
+    public void NullInput()
+    {
+        const string expression = null;
+        try
+        {
+            var result = DiceRoller.Roll(expression);
             Assert.Fail();
         }
-
-        [Test]
-        public void BadInput()
+        catch (DiceRollException)
         {
-            const string expression = "This is not a dice roll expression";
-            try
-            {
-                var result = DiceRoller.Roll(expression);
-                Assert.Fail();
-            }
-            catch (DiceRollException)
-            {
-                Assert.Pass();
-            }
+            Assert.Pass();
+        }
+    }
 
+    [Test]
+    public void BadInput()
+    {
+        const string expression = "This is not a dice roll expression";
+        try
+        {
+            var result = DiceRoller.Roll(expression);
             Assert.Fail();
         }
-
-        [Test]
-        public void ChainExpression()
+        catch (DiceRollException)
         {
-            // NOTE(istarnion): This is sort of an unintended feature.
-            // discuss with users and see if this is actually something
-            // we should reject as invalid syntax.
-            const string expression = "1d1 - 1d1d100d100d100";
+            Assert.Pass();
+        }
+    }
+
+    [Test]
+    public void ChainExpression()
+    {
+        // NOTE(istarnion): This is sort of an unintended feature.
+        // discuss with users and see if this is actually something
+        // we should reject as invalid syntax.
+        const string expression = "1d1 - 1d1d100d100d100";
+        var result = DiceRoller.Roll(expression);
+        Assert.Positive(result.Total);
+    }
+
+    [Test]
+    public void NegativeExpression()
+    {
+        const string expression = "-1d20";
+        try
+        {
+            var result = DiceRoller.Roll(expression);
+            Assert.Negative(result.Total);
+        }
+        catch
+        {
+            Assert.Fail();
+        }
+    }
+
+    [Test]
+    public void ExplicitPositiveExpression()
+    {
+        const string expression = "+1d20";
+        try
+        {
             var result = DiceRoller.Roll(expression);
             Assert.Positive(result.Total);
         }
-
-        [Test]
-        public void NegativeExpression()
+        catch
         {
-            const string expression = "-1d20";
-            try
-            {
-                var result = DiceRoller.Roll(expression);
-                Assert.Negative(result.Total);
-            }
-            catch
-            {
-                Assert.Fail();
-            }
-        }
-
-        [Test]
-        public void ExplicitPositiveExpression()
-        {
-            const string expression = "+1d20";
-            try
-            {
-                var result = DiceRoller.Roll(expression);
-                Assert.Positive(result.Total);
-            }
-            catch
-            {
-                Assert.Fail();
-            }
+            Assert.Fail();
         }
     }
 }
--- Dicebag/DiceRoller.cs
+++ Dicebag/DiceRoller.cs
@@ -1,272 +1,269 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Dicebag
+namespace Dicebag;
+
+public sealed class DiceRollException : Exception
 {
-    public class DiceRollException : Exception
+    public string Expression { get; init; }
+    public int Offset { get; init; }
+
+    public DiceRollException(string message, string expression, int offset) : base(message)
     {
-        public string Expression { get; private set; }
-        public int Offset { get; private set; }
-
-        public DiceRollException(string message, string expression, int offset) : base(message)
-        {
-            Expression = expression ?? "";
-            Offset = offset;
-        }
+        Expression = expression ?? "";
+        Offset = offset;
     }
+}
 
+/// <summary>
+/// Simple tool for rolling dice expressions such as 1d20, 4d6+2 etc
+/// </summary>
+/// <remarks>
+/// Dice expression BNF:
+///            [roll] ::= (+|-)?[expression]((+|-)[expression])*
+///      [expression] ::= [number]|[dice-expression]
+/// [dice-expression] ::= [number]?d[number]
+///
+/// Any whitespace is ignored. Both 'd' and 'D' are supported.
+/// </remarks>
+public static class DiceRoller
+{
     /// <summary>
-    /// Simple tool for rolling dice expressions such as 1d20, 4d6+2 etc
+    /// Holds the result of a roll.
     /// </summary>
-    /// <remarks>
-    /// Dice expression BNF:
-    ///            [roll] ::= (+|-)?[expression]((+|-)[expression])*
-    ///      [expression] ::= [number]|[dice-expression]
-    /// [dice-expression] ::= [number]?d[number]
-    ///
-    /// Any whitespace is ignored. Both 'd' and 'D' are supported.
-    /// </remarks>
-    public static class DiceRoller
+    public sealed class Result
     {
         /// <summary>
-        /// Holds the result of a roll.
+        /// The total sum of the roll
         /// </summary>
-        public class Result
+        public int Total { get; set; }
+
+        /// <summary>
+        /// The total modifier that was applied to the roll(s)
+        /// </summary>
+        public int Modifier { get; set; }
+
+        /// <summary>
+        /// The results of the individual rolls.
+        /// The key is the die type, "d20", "d6", etc
+        /// The value is a list of the results of every roll with that die
+        /// </summary>
+        public Dictionary<string, List<int>> Rolls { get; set; }
+    }
+
+    private sealed class ExpressionParseResult
+    {
+        public int Constant;
+        public int NumDice;
+        public int DiceFaces;
+    }
+
+    public static Result Roll(string expression)
+    {
+        if (string.IsNullOrWhiteSpace(expression))
         {
-            /// <summary>
-            /// The total sum of the roll
-            /// </summary>
-            public int Total { get; set; }
-
-            /// <summary>
-            /// The total modifier that was applied to the roll(s)
-            /// </summary>
-            public int Modifier { get; set; }
-
-            /// <summary>
-            /// The results of the individual rolls.
-            /// The key is the die type, "d20", "d6", etc
-            /// The value is a list of the results of every roll with that die
-            /// </summary>
-            public Dictionary<string, List<int>> Rolls { get; set; }
+            throw new DiceRollException("[ERROR]: The roll expression is empty.", expression, 0);
         }
 
-        private class ExpressionParseResult
+        char[] diceExpression = expression.Trim().ToCharArray();
+
+        for(int i=0; i<diceExpression.Length; ++i)
         {
-            public int Constant;
-            public int NumDice;
-            public int DiceFaces;
+            char c = diceExpression[i];
+            if(c == 'D')
+            {
+                diceExpression[i] = 'd';
+            }
         }
 
-        public static Result Roll(string expression)
+        int offset = 0;
+
+        void Panic(string errorMessage)
         {
-            if (string.IsNullOrWhiteSpace(expression))
+            throw new DiceRollException(errorMessage, expression, offset);
+        }
+
+        char Peek()
+        {
+            return diceExpression[offset];
+        }
+
+        char Pop()
+        {
+            char result = diceExpression[offset];
+            ++offset;
+            return result;
+        }
+
+        bool EOF()
+        {
+            return offset >= diceExpression.Length;
+        }
+
+        void GobbleWhitespace()
+        {
+            while (!EOF() && char.IsWhiteSpace(Peek()))
             {
-                throw new DiceRollException("[ERROR]: The roll expression is empty.", expression, 0);
-            }
-
-            char[] diceExpression = expression.Trim().ToCharArray();
-
-            for(int i=0; i<diceExpression.Length; ++i)
-            {
-                char c = diceExpression[i];
-                if(c == 'D')
-                {
-                    diceExpression[i] = 'd';
-                }
-            }
-
-            int offset = 0;
-
-            void Panic(string errorMessage)
-            {
-                throw new DiceRollException(errorMessage, expression, offset);
-            }
-
-            char Peek()
-            {
-                return diceExpression[offset];
-            }
-
-            char Pop()
-            {
-                char result = diceExpression[offset];
                 ++offset;
-                return result;
+            }
+        }
+
+        bool Expect(char c)
+        {
+            GobbleWhitespace();
+            if (diceExpression[offset] == c)
+            {
+                ++offset;
+                return true;
             }
 
-            bool EOF()
+            return false;
+        }
+
+
+        int ConsumeNumber()
+        {
+            int result = 0;
+            GobbleWhitespace();
+            while (!EOF() && char.IsDigit(Peek()))
             {
-                return offset >= diceExpression.Length;
+                char c = Pop();
+                int i = c - '0';
+                result = result * 10 + i;
             }
 
-            void GobbleWhitespace()
+            return result;
+        }
+
+        int ExpectNonZeroNumber()
+        {
+            int result = ConsumeNumber();
+
+            if (result <= 0)
             {
-                while (!EOF() && char.IsWhiteSpace(Peek()))
+                Panic("Expected a non-zero number.");
+            }
+
+            return result;
+        }
+
+        // Can be just [number] or [number]?d[number]
+        ExpressionParseResult ConsumeExpression()
+        {
+            var result = new ExpressionParseResult();
+
+            GobbleWhitespace();
+            if (char.IsDigit(Peek()))
+            {
+                int number = ConsumeNumber();
+                GobbleWhitespace();
+
+                if (!EOF() && Peek() == 'd')
                 {
                     ++offset;
-                }
-            }
-
-            bool Expect(char c)
-            {
-                GobbleWhitespace();
-                if (diceExpression[offset] == c)
-                {
-                    ++offset;
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-
-
-            int ConsumeNumber()
-            {
-                int result = 0;
-                GobbleWhitespace();
-                while (!EOF() && char.IsDigit(Peek()))
-                {
-                    char c = Pop();
-                    int i = c - '0';
-                    result = result * 10 + i;
-                }
-
-                return result;
-            }
-
-            int ExpectNonZeroNumber()
-            {
-                int result = ConsumeNumber();
-
-                if (result <= 0)
-                {
-                    Panic("Expected a non-zero number.");
-                }
-
-                return result;
-            }
-
-            // Can be just [number] or [number]?d[number]
-            ExpressionParseResult ConsumeExpression()
-            {
-                var result = new ExpressionParseResult();
-
-                GobbleWhitespace();
-                if (char.IsDigit(Peek()))
-                {
-                    int number = ConsumeNumber();
-                    GobbleWhitespace();
-
-                    if (!EOF() && Peek() == 'd')
-                    {
-                        ++offset;
-                        int number2 = ExpectNonZeroNumber();
-
-                        result.NumDice = number;
-                        result.DiceFaces = number2;
-                    }
-                    else
-                    {
-                        result.Constant = number;
-                    }
-                }
-                else if (Expect('d'))
-                {
                     int number2 = ExpectNonZeroNumber();
 
-                    result.NumDice = 1;
+                    result.NumDice = number;
                     result.DiceFaces = number2;
                 }
                 else
                 {
-                    Panic("Expected dice expression.");
-                }
-
-                return result;
-            }
-
-            var rng = new Random();
-            var result = new Result();
-            result.Rolls = new Dictionary<string, List<int>>();
-
-            int RollDice(int numFaces)
-            {
-                int result = 1 + rng.Next(numFaces);
-                return result;
-            }
-
-            int sign = 1; // The sign of the "connecting operator"
-
-            // Expressions might start with an '-' or an explicit '+'
-            // that we should allow.
-            if(Peek() == '-')
-            {
-                sign = -1;
-                ++offset;
-            }
-            else if(Peek() == '+')
-            {
-                ++offset;
-            }
-
-            do
-            {
-                var expressionResult = ConsumeExpression();
-
-                if (expressionResult.NumDice > 0)
-                {
-                    string dieKey = "d" + expressionResult.DiceFaces;
-                    var results = new List<int>(expressionResult.NumDice);
-                    for (int i = 0; i < expressionResult.NumDice; ++i)
-                    {
-                        int roll = RollDice(expressionResult.DiceFaces);
-                        results.Add(roll);
-                        result.Total += roll * sign;
-                    }
-
-                    if (result.Rolls.ContainsKey(dieKey))
-                    {
-                        result.Rolls[dieKey].AddRange(results);
-                    }
-                    else
-                    {
-                        result.Rolls[dieKey] = results;
-                    }
-                }
-                else
-                {
-                    result.Modifier += expressionResult.Constant * sign;
-                }
-
-                GobbleWhitespace();
-                if (!EOF())
-                {
-                    if (Peek() == '+')
-                    {
-                        ++offset;
-                        sign = 1;
-                    }
-                    else if (Peek() == '-')
-                    {
-                        ++offset;
-                        sign = -1;
-                    }
-                    else
-                    {
-                        // NOTE(istarnion): Panic here if we
-                        // don't want to allow chain expressions
-                        sign = 1;
-                    }
+                    result.Constant = number;
                 }
             }
-            while (!EOF());
+            else if (Expect('d'))
+            {
+                int number2 = ExpectNonZeroNumber();
 
-            result.Total += result.Modifier;
+                result.NumDice = 1;
+                result.DiceFaces = number2;
+            }
+            else
+            {
+                Panic("Expected dice expression.");
+            }
 
             return result;
         }
+
+        var rng = new Random();
+        var result = new Result();
+        result.Rolls = new Dictionary<string, List<int>>();
+
+        int RollDice(int numFaces)
+        {
+            int result = 1 + rng.Next(numFaces);
+            return result;
+        }
+
+        int sign = 1; // The sign of the "connecting operator"
+
+        // Expressions might start with an '-' or an explicit '+'
+        // that we should allow.
+        if(Peek() == '-')
+        {
+            sign = -1;
+            ++offset;
+        }
+        else if(Peek() == '+')
+        {
+            ++offset;
+        }
+
+        do
+        {
+            var expressionResult = ConsumeExpression();
+
+            if (expressionResult.NumDice > 0)
+            {
+                string dieKey = "d" + expressionResult.DiceFaces;
+                var results = new List<int>(expressionResult.NumDice);
+                for (int i = 0; i < expressionResult.NumDice; ++i)
+                {
+                    int roll = RollDice(expressionResult.DiceFaces);
+                    results.Add(roll);
+                    result.Total += roll * sign;
+                }
+
+                if (result.Rolls.ContainsKey(dieKey))
+                {
+                    result.Rolls[dieKey].AddRange(results);
+                }
+                else
+                {
+                    result.Rolls[dieKey] = results;
+                }
+            }
+            else
+            {
+                result.Modifier += expressionResult.Constant * sign;
+            }
+
+            GobbleWhitespace();
+            if (!EOF())
+            {
+                if (Peek() == '+')
+                {
+                    ++offset;
+                    sign = 1;
+                }
+                else if (Peek() == '-')
+                {
+                    ++offset;
+                    sign = -1;
+                }
+                else
+                {
+                    // NOTE(istarnion): Panic here if we
+                    // don't want to allow chain expressions
+                    sign = 1;
+                }
+            }
+        }
+        while (!EOF());
+
+        result.Total += result.Modifier;
+
+        return result;
     }
 }
